157cdba7c251a4ff98661d5043a898f6b2d57aaa
InfiniteCoder <info@infinitecoder.org>
2025-12-16 16:19:44 +0300

    Change UI layout

--- frontend/ui.js
+++ frontend/ui.js
@@ -1,13 +1,30 @@
 class BaseUI {
     static itemsTexture = texture("ui/items.png");
     static itemTile = 16;
+    static background = texture("ui/inventory.png");
 
     drawItem(id, count, x, y) {
         drawTile(BaseUI.itemsTexture, Number(id), BaseUI.itemTile, x, y)
         if (count) {
             ctx.font = "5px editundo";
             ctx.fillStyle = "white";
-            ctx.fillText(count, x + BaseUI.itemTile + 3 - ctx.measureText(count).width, y + BaseUI.itemTile);
+            ctx.fillText(count, x + BaseUI.itemTile - ctx.measureText(count).width, y + BaseUI.itemTile);
+        }
+    }
+
+    drawBaseInventory(x, y) {
+        ctx.drawImage(InventoryUI.background, x, y);
+        let index = 0;
+        for (const [item, count] of Object.entries(player.inventory)) {
+            const col = index % 5;
+            const row = Math.floor(index / 5);
+            this.drawItem(
+                item,
+                count == 1 ? null : count,
+                x + 16 + col * 20,
+                y + 16 + row * 20
+            );
+            index++;
         }
     }
 
@@ -16,17 +33,10 @@ class BaseUI {
 }
 
 class InventoryUI extends BaseUI {
-    static background = texture("ui/inventory.png");
-
     draw(width, height) {
-        const dx = (width - InventoryUI.background.width) / 2;
-        const dy = (height - InventoryUI.background.height) / 2;
-        ctx.drawImage(InventoryUI.background, dx, dy);
-        let index = 0;
-        for (const [item, count] of Object.entries(player.inventory)) {
-            this.drawItem(item, count == 1 ? null : count, dx + 20 + index * (1 + BaseUI.itemTile), dy + 19);
-            index++;
-        }
+        const dx = (width - BaseUI.background.width) / 2;
+        const dy = (height - BaseUI.background.height) / 2;
+        this.drawBaseInventory(dx, dy);
     }
 }
 
